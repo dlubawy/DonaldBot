5b4246e38a0faea653bd80ce27cb85144ed47845
Andrew Lubawy <dlubawy@gmail.com>
2016-05-24 12:19:28 -0400

    added bot

---
+++ README.md
@@ -0,0 +1,4 @@
+#Donald Trump Bot
+##About
+This is just a side project to create a GroupMe bot that scrapes 
+@realDonaldTrump's Twitter and picks a random tweet to post to a group chat.
---
+++ donald_tweets.py
@@ -0,0 +1,22 @@
+import twitter
+import os
+import random
+import urllib2
+import json
+
+api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'),
+                  consumer_secret=os.getenv('CONSUMER_SECRET'),
+                  access_token_key=os.getenv('ACCESS_TOKEN'),
+                  access_token_secret=os.getenv('ACCESS_SECRET'))
+
+statuses = api.GetUserTimeline(screen_name='realDonaldTrump')
+
+bot_id = os.getenv('BOT_ID')
+rand = random.randint(0,20)
+data = {
+        "bot_id" : bot_id,
+        "text" : statuses[rand].text
+        }
+req = urllib2.Request('https://api.groupme.com/v3/bots/post')
+req.add_header('Content-Type', 'application/json')
+response = urllib2.urlopen(req, json.dumps(data))
