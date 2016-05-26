b673c8b5e1098cb7cc7bbc0e2a38a2a59b88a71a
Andrew Lubawy <dlubawy@gmail.com>
2016-05-26 09:50:38 -0400

    Updated settings

--- DonaldBot/settings.py
+++ DonaldBot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'b=b$$xkit66u0@sc#02njs0^vabty1aljxv1kp@$qlzv^a73)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aqueous-hamlet-55578.herokuapp.com']
 
