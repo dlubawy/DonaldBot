8a9cf218e4424b6c2f495955de5409004f58a2aa
Andrew Lubawy <dlubawy@gmail.com>
2016-05-27 09:59:36 -0400

    Added another command

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-import json
-import imp
+import json, imp, random
 donald_bot = imp.load_source('donald_bot', 'bot/donald_bot.py')
 
 @csrf_exempt
@@ -12,8 +11,18 @@ def index(request):
     sender = json_data['name']
     
     name = '@thedonald'
-    
-    if name in text.lower():
+    command = '@thedonald how'
+
+    if command in text.lower():
+        s = text.split()
+        minus_punct = s[4][:len(s[4])-1]
+        rand = random.randint(0,100)
+        if '?' in s[4]:
+            donald_bot.send("{0} is {1}% {2}".format(minus_punct, rand, s[2]))
+        else:
+            donald_bot.send("{0} is {1}% {2}".format(s[4], rand, s[2]))
+    elif name in text.lower():
         donald_bot.tweet()
+
     if 'wall' in text.lower():
         donald_bot.send("I'll build it higher and make %s pay for it." % sender)
