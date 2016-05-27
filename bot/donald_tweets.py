904e332b77176b31a621285cadcd5c4941ab386f
Andrew Lubawy <dlubawy@gmail.com>
2016-05-27 08:09:59 -0400

    Added callback function

--- bot/donald_tweets.py
+++ bot/donald_tweets.py
@@ -5,18 +5,19 @@ import urllib2
 import json
 
 api = twitter.Api(consumer_key=os.getenv('CONSUMER_KEY'),
-                  consumer_secret=os.getenv('CONSUMER_SECRET'),
-                  access_token_key=os.getenv('ACCESS_TOKEN'),
-                  access_token_secret=os.getenv('ACCESS_SECRET'))
-
-statuses = api.GetUserTimeline(screen_name='realDonaldTrump')
+        consumer_secret=os.getenv('CONSUMER_SECRET'),
+        access_token_key=os.getenv('ACCESS_TOKEN'),
+        access_token_secret=os.getenv('ACCESS_SECRET'))
 
 bot_id = os.getenv('BOT_ID')
-rand = random.randint(0,20)
-data = {
-        "bot_id" : bot_id,
-        "text" : statuses[rand].text
-        }
-req = urllib2.Request('https://api.groupme.com/v3/bots/post')
-req.add_header('Content-Type', 'application/json')
-response = urllib2.urlopen(req, json.dumps(data))
+
+def main():
+    statuses = api.GetUserTimeline(screen_name='realDonaldTrump')
+    rand = random.randint(0,20)
+    data = {
+            "bot_id" : bot_id,
+            "text" : statuses[rand].text
+            }
+    req = urllib2.Request('https://api.groupme.com/v3/bots/post')
+    req.add_header('Content-Type', 'application/json')
+    response = urllib2.urlopen(req, json.dumps(data))
--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-import donald_tweets
+import imp
+donald_tweets = imp.load_source('donald_tweets', 'bot/donald_tweets.py')
 
 def index(request):
     text = request.POST['text']
     name = '@TheDonald'
-    if(name.find(text, beg=0 end=len(text)) != -1):
-        donald_tweets
+    if(name.find(text, beg=0, end=len(text)) != -1):
+        main()
